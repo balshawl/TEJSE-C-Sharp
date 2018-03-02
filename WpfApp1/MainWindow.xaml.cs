244579d935dd10eb56c784c37fffcac82be1df82
balshawl <l.balshaw@lancaster.ac.uk>
2018-03-02 00:12:27 +0000

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SaveFile f;
+        private Boolean changed = false;
+        private String title = "Italian Job Save Editor";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            Close();
+
+
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "res_data.dat"; // Default file name
+            dlg.DefaultExt = ".dat"; // Default file extension
+            dlg.Filter = "Text documents|*.dat"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                mainEditor.IsEnabled = true;
+                all.IsEnabled = true;
+                f = new SaveFile(filename);
+                SetTicked();
+                if (allChecked())
+                {
+                    setAllChecked(true);
+
+                }
+                
+            }
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void change() {
+            changed = true;
+            Saver.IsEnabled = true;
+            this.Title = title + "*";
+        }
+
+        private void SetTicked()
+        {
+            Boolean[] IJSet = f.isIJMissionsSet();
+            if (IJSet[0] == true)
+            {
+                ijlondon.IsChecked = true;
+            }
+            if (IJSet[1] == true)
+            {
+                ijturin.IsChecked = true;
+            }
+            if (IJSet[2] == true)
+            {
+                ijalps.IsChecked = true;
+            }
+            Boolean[] DSet = f.isDMissionsSet();
+            if (DSet[0] == true)
+            {
+                dlondon.IsChecked = true;
+            }
+            if (DSet[1] == true)
+            {
+                dturin.IsChecked = true;
+            }
+            Boolean[] CSet = f.isCMissionsSet();
+            if (CSet[0] == true)
+            {
+                clondon.IsChecked = true;
+            }
+            if (CSet[1] == true)
+            {
+                cturin.IsChecked = true;
+            }
+            
+
+            Boolean[] ChallengeSet = f.isChallengeMissionsSet();
+            if (ChallengeSet[0] == true)
+            {
+                challenges.IsChecked = true;
+            }
+            //setAllChecked();
+        }
+
+        private void Challenges(object sender, RoutedEventArgs e)
+        {
+
+
+            bool b;
+            if (((CheckBox)e.Source).IsChecked == true)
+            {
+                b = true;
+            }
+            else
+            {
+                b = false;
+            }
+            if (e.Source.Equals(challenges))
+            {
+                f.setChallengeMissions(b, 0);
+            }
+        }
+
+        private void Checkpoint(object sender, RoutedEventArgs e)
+        {
+
+
+            bool b;
+            if (((CheckBox)e.Source).IsChecked == true)
+            {
+                b = true;
+            }
+            else
+            {
+                b = false;
+            }
+            if (e.Source.Equals(clondon))
+            {
+                f.setCheckpointMissions(b, 0);
+
+            }
+            else if (e.Source.Equals(cturin))
+            {
+                f.setCheckpointMissions(b, 1);
+
+            }
+        }
+
+        private void Destructor(object sender, RoutedEventArgs e)
+        {
+
+            bool b;
+            if (((CheckBox)sender).IsChecked == true)
+            {
+                b = true;
+            }
+            else
+            {
+                b = false;
+            }
+            if (sender.Equals(dlondon))
+            {
+                f.setDestructorMissions(b, 0);
+
+            }
+            else if (sender.Equals(dturin))
+            {
+                f.setDestructorMissions(b, 1);
+
+            }
+
+        }
+
+        private void ItalianJob(object sender, RoutedEventArgs e)
+        {
+
+            bool b;
+            if (((CheckBox)sender).IsChecked == true)
+            {
+                b = true;
+            }
+            else
+            {
+                b = false;
+            }
+            if (sender.Equals(ijlondon))
+            {
+                f.setIJMissions(b, 0);
+
+            }
+            else if (sender.Equals(ijturin))
+            {
+                f.setIJMissions(b, 1);
+
+            }
+            else if (sender.Equals(ijalps))
+            {
+                f.setIJMissions(b, 2);
+            }
+        }
+
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+
+        private Boolean allChecked()
+        {
+            Boolean isChecked = true;
+            foreach (CheckBox tb in FindVisualChildren<CheckBox>(this))
+            {
+                if (tb.IsChecked == false && !tb.Name.Equals("all"))
+                {
+                    isChecked = false;
+                    break;
+
+                }
+            }
+            return isChecked;
+        }
+
+        private void setAllChecked (Boolean set){
+            if(set == true)
+            {
+                foreach (GroupBox tb in FindVisualChildren<GroupBox>(mainEditor))
+                {
+                    
+                    tb.IsEnabled = false;
+                    
+                }
+                foreach (CheckBox tb in FindVisualChildren<CheckBox>(mainEditor))
+                {
+                    tb.IsChecked = true;
+
+                }
+
+                all.IsChecked = true;
+
+            }
+            else
+            {
+                foreach (GroupBox tb in FindVisualChildren<GroupBox>(mainEditor))
+                {
+
+                    tb.IsEnabled = true;
+
+                }
+                foreach (CheckBox tb in FindVisualChildren<CheckBox>(mainEditor))
+                {
+                    tb.IsChecked = false;
+
+                }
+
+                all.IsChecked = false;
+            }
+        }
+
+        private void Checkbox_Click(object sender, RoutedEventArgs e)
+        {
+            change();
+            if (!((CheckBox)sender).Name.Equals("all"))
+            {
+                if (allChecked())
+                {
+                    setAllChecked(true);
+                }
+               
+            }
+            
+            
+
+        }
+
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            f.Write();
+            changed = false;
+            Saver.IsEnabled = false;
+            this.Title = title;
+        }
+        private void OnClosing(object sender, CancelEventArgs e)
+        {
+            
+            if (changed == true)
+            {
+                MessageBoxResult result = MessageBox.Show("Would you like to save changes to the file before exiting?", title, MessageBoxButton.YesNoCancel);
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        f.Write();
+                       
+                        break;
+                    case MessageBoxResult.No:
+                       
+                        break;
+                    case MessageBoxResult.Cancel:
+                        e.Cancel = true;
+
+                        break;
+                }
+
+            }
+            else
+            {
+                
+
+            }
+            //Do whatever you want here..
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            bool b;
+            if (((CheckBox)sender).IsChecked == true)
+            {
+                b = true;
+            }
+            else
+            {
+                b = false;
+            }
+
+            if(b == true)
+            {
+                setAllChecked(true);
+            }
+            else
+            {
+                setAllChecked(false);
+            }
+            f.setAll(b,0);
+
+        }
+    }
+
+    
+}
---
+++ WpfApp1/SaveFile.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class SaveFile
+    {
+        byte[] saveTemp;
+        public static int IJ_LONDON_MISSIONS = 0;
+        public static int IJ_TURIN_MISSIONS = 1;
+        public static int IJ_ALPS_MISSIONS = 2;
+
+
+        int[] IJLondon = { 16, 12, 14, 20, 18, 26, 22 };
+        int[] IJTurin = { 24,152, 150, 156, 154, 158 };
+        int[] IJAlps = { 162, 160 };
+        ArrayList ItalianJobMissions = new ArrayList();
+
+        int[] CLondon = { 48, 52, 56, 44, 42, 64, 40, 60, 62 };
+        int[] CTurin = {72, 74, 76, 78, 80, 82, 84, 86, 88};
+        ArrayList CheckpointMissions = new ArrayList();
+
+        int[] DLondon = { 108, 106, 100, 94, 34, 102, 96, 32, 104, 92 };
+        int[] DTurin = { 194, 190, 192, 196, 204, 198, 168, 200, 202, 206 };
+        ArrayList DestructorMissions = new ArrayList();
+
+        int[] Challenges = { 38, 50, 36, 46, 30, 186, 70, 188, 68 };
+        ArrayList ChallengeMissions = new ArrayList();
+
+        int[] all = {166 };
+        ArrayList allList = new ArrayList();
+
+
+        String fileName;
+        
+
+        public SaveFile(String fileName)
+        {
+            this.fileName = fileName;
+            ItalianJobMissions.Add(IJLondon);
+            ItalianJobMissions.Add(IJTurin);
+            ItalianJobMissions.Add(IJAlps);
+
+            CheckpointMissions.Add(CLondon);
+            CheckpointMissions.Add(CTurin);
+
+            DestructorMissions.Add(DLondon);
+            DestructorMissions.Add(DTurin);
+
+            ChallengeMissions.Add(Challenges);
+
+            allList.Add(all);
+
+            saveTemp = File.ReadAllBytes(fileName);
+            System.Console.WriteLine("Read file. Length of: " + saveTemp.Length);
+            foreach (Byte a in saveTemp)
+            {
+                System.Console.Write((int)a);
+            }
+        }
+
+        public Boolean[] isAllSet()
+        {
+            Boolean[] set = { true };
+
+            foreach (int[] b in allList)
+            {
+                foreach (int a in b)
+                {
+                    if (saveTemp[a] == 0)
+                    {
+                        set[allList.IndexOf(b)] = false;
+                        break;
+                    }
+                }
+            }
+            return set;
+        }
+
+        public void setAll(Boolean set, int i)
+        {
+            System.Console.WriteLine(set);
+            foreach (int a in (int[])allList[i])
+            {
+
+                if (set == true)
+                {
+                    saveTemp[a] = 1;
+                }
+                else
+                {
+                    saveTemp[a] = 0;
+                }
+            }
+        }
+
+
+
+        public Boolean[] isChallengeMissionsSet()
+        {
+            Boolean[] set = { true};
+
+            foreach (int[] b in ChallengeMissions)
+            {
+                foreach (int a in b)
+                {
+                    if (saveTemp[a] == 0)
+                    {
+                        set[ChallengeMissions.IndexOf(b)] = false;
+                        break;
+                    }
+                }
+            }
+            return set;
+        }
+
+        public void setChallengeMissions(Boolean set, int i)
+        {
+            System.Console.WriteLine(set);
+            foreach (int a in (int[])ChallengeMissions[i])
+            {
+
+                if (set == true)
+                {
+                    saveTemp[a] = 1;
+                }
+                else
+                {
+                    saveTemp[a] = 0;
+                }
+            }
+        }
+
+        public Boolean[] isDMissionsSet()
+        {
+            Boolean[] set = { true, true };
+
+            foreach (int[] b in DestructorMissions)
+            {
+                foreach (int a in b)
+                {
+                    if (saveTemp[a] == 0)
+                    {
+                        set[DestructorMissions.IndexOf(b)] = false;
+                        break;
+                    }
+                }
+            }
+            return set;
+        }
+
+        public void setDestructorMissions(Boolean set, int i)
+        {
+            System.Console.WriteLine(set);
+            foreach (int a in (int[])DestructorMissions[i])
+            {
+
+                if (set == true)
+                {
+                    saveTemp[a] = 1;
+                }
+                else
+                {
+                    saveTemp[a] = 0;
+                }
+            }
+        }
+
+        public Boolean[] isCMissionsSet()
+        {
+            Boolean[] set = { true, true };
+
+            foreach (int[] b in CheckpointMissions)
+            {
+                foreach (int a in b)
+                {
+                    if (saveTemp[a] == 0)
+                    {
+                        set[CheckpointMissions.IndexOf(b)] = false;
+                        break;
+                    }
+                }
+            }
+            return set;
+        }
+
+        public void setCheckpointMissions(Boolean set, int i)
+        {
+            System.Console.WriteLine(set);
+            foreach (int a in (int[])CheckpointMissions[i])
+            {
+
+                if (set == true)
+                {
+                    saveTemp[a] = 1;
+                }
+                else
+                {
+                    saveTemp[a] = 0;
+                }
+            }
+        }
+
+        public Boolean[] isIJMissionsSet()
+        {
+            Boolean[] set = { true, true, true };
+            
+            foreach(int[] b in ItalianJobMissions){
+                foreach (int a in b)
+                {
+                    if (saveTemp[a] == 0)
+                    {
+                        set[ItalianJobMissions.IndexOf(b)] = false;
+                        break;
+                    }
+                }
+            }
+            return set;
+        }
+
+        public void setIJMissions(Boolean set, int i)
+        {
+            System.Console.WriteLine(set);
+            foreach(int a in (int[])ItalianJobMissions[i])
+            {
+
+                if(set == true)
+                {
+                    saveTemp[a] = 1;
+                }
+                else
+                {
+                    saveTemp[a] = 0;
+                }   
+            } 
+        }
+
+        public void Write()
+        {
+            var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            fs.Write(saveTemp, 0, saveTemp.Length);
+            fs.Close();
+        }
+
+
+
+
+    }
+}
